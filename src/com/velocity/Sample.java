a8e5e73402e7794c38d47335ab5173e9848c46a9
shital <shitaljadhav@2194@gamil.com>
2023-01-19 14:30:32 -0800

    First commit

---
+++ src/com/velocity/Sample.java
@@ -0,0 +1,26 @@
+package com.velocity;
+
+import java.util.*;
+
+
+public class Sample {
+	public static void main(String[] args) {
+		
+		List<Integer> list = new ArrayList<Integer>();
+		
+		list.add(2);
+		list.add(4);
+		list.add(7);
+		list.add(8);
+		
+		System.out.println(list);
+	
+	}
+	
+	}
+	
+	
+	
+	
+
+
